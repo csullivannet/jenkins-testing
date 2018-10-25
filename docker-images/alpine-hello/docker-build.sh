34db66d5c8de7507571435696e39993146f81ed2
Christopher Sullivan <csullivannet@users.noreply.github.com>
2018-10-25 10:15:46 -0400

    Junk random

--- docker-images/alpine-hello/docker-build.sh
+++ docker-images/alpine-hello/docker-build.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 DISTRO=alpine
 
-cp -R ../../${DISTRO}/text .
+SOURCE_DIR=$(cd $(dirname "$0")/../..; pwd -P)
+
+cd $(mktemp -d /tmp/${DISTRO}-build.XXXXX)
+
+cp -R ${SOURCE_DIR}/text .
 
 docker build -t ${DISTRO}-hello .
