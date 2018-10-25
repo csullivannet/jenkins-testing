6822ec26e76f15255acea3e684cbe0028965a892
Christopher Sullivan <csullivannet@users.noreply.github.com>
2018-10-25 10:17:05 -0400

    not actually that random

--- docker-images/debian-hello/docker-build.sh
+++ docker-images/debian-hello/docker-build.sh
@@ -5,6 +5,8 @@ SOURCE_DIR=$(cd $(dirname "$0")/../..; pwd -P)
 
 cd $(mktemp -d /tmp/${DISTRO}-build.XXXXX)
 
+cp ${SOURCE_DIR}/Dockerfile .
+
 cp -R ${SOURCE_DIR}/text .
 
 docker build -t ${DISTRO}-hello .
